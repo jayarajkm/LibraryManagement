9f23357090eef95bb303a3b8c47bf8ca938d67bd
JMadhusoodhanan <JMadhusoodhanan@psmicorp.com>
2025-10-07 18:13:36 +0530

    initial Commit

---
+++ LibraryManagementApi/Models/Book.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace LibraryManagementApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public int YearPublished { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public int? BorrowerId { get; set; }
+        public Member? Borrower { get; set; }
+    }
+}
+
+
